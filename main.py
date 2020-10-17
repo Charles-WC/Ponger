6f34eb4dfeffa9593f652c2596da203474590f68
Charles <charles@mutuallyassured.com>
2020-10-16 21:44:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,35 +13,34 @@ FRAME_RATE = 60
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
+PADDLE_STATE_STILL = "still"
+PADDLE_STATE_MOVE = "moving"
+
 
 class Ball(object):
 
     def __init__(self, screen):
 
-        self.x_direction = DIRECTION_NONE
-        self.y_direction = DIRECTION_UP
+        self.radius = 10
         self.screen = screen
-        self.x_speed = 300
-        self.y_speed = 100
         self.state = BALL_STATE_READY
-        self.radius = 14
         self.x = 0
         self.y = 0
+        self.x_direction = DIRECTION_NONE
+        self.y_direction = DIRECTION_UP
+        self.x_speed = 300
+        self.y_speed = 100
 
     def draw(self):
         pygame.draw.circle(self.screen, (227, 232, 41), (int(self.x), int(self.y)), self.radius)
 
-    def move(self, time_delta, direction=""):
+    def move(self, time_delta):
 
         # First, let's GTFO if we are already doing something
         if self.state is not BALL_STATE_FIRING:
             return None
 
-        # What direction are we going?
-        if direction is not None and self.x_direction is not direction:
-            self.x_direction = direction
-
-        if direction == DIRECTION_LEFT:
+        if self.x_direction == DIRECTION_LEFT:
             pixels_to_move_x = -self.x_speed
         else:
             pixels_to_move_x = self.x_speed
@@ -54,19 +53,38 @@ class Ball(object):
         self.x += time_delta * pixels_to_move_x
         self.y += time_delta * pixels_to_move_y
 
+    def moveCenter(self, new_x):
+        
+
 
 class Paddle(object):
 
     def __init__(self, screen):
         self.direction = DIRECTION_NONE
+        self.image = pygame.image.load("res/paddle.png")
         self.screen = screen
-        self.width = 100
+        self.speed = 300
+        self.state = PADDLE_STATE_STILL
+        self.width = 200
         self.height = 25
         self.x = 0
         self.y = 0
 
     def draw(self):
-        pygame.draw.rect(self.screen, (208, 206, 206), (self.x, self.y, self.width, self.height))
+        self.screen.blit(self.image, (self.x, self.y))
+
+    def move(self, time_delta):
+
+        # Check the direction
+        if self.state == PADDLE_STATE_STILL:
+            return
+
+        if self.direction == DIRECTION_LEFT:
+            pixels_to_move_x = -self.speed
+        else:
+            pixels_to_move_x = self.speed
+
+        self.x += time_delta * pixels_to_move_x
 
 
 pygame.init()
@@ -95,6 +113,16 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 ball.state = BALL_STATE_FIRING
+        elif event.type == pygame.KEYUP:
+            paddle.state = PADDLE_STATE_STILL
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        paddle.direction = DIRECTION_LEFT
+        paddle.state = PADDLE_STATE_MOVE
+    elif keys[pygame.K_RIGHT]:
+        paddle.direction = DIRECTION_RIGHT
+        paddle.state = PADDLE_STATE_MOVE
 
     # FIll up the background
     screen.fill((25, 25, 25))
@@ -105,6 +133,13 @@ while True:
     elif ball.x >= SCREEN_WIDTH - ball.radius:
         ball.x_direction = DIRECTION_LEFT
 
+    if ball.y <= 0:
+        ball.y_direction = DIRECTION_DOWN
+    elif ball.y >= SCREEN_HEIGHT - ball.radius:
+        ball.y_direction = DIRECTION_UP
+
+    ball.move(time_delta)
+    paddle.move(time_delta)
     paddle.draw()
     ball.draw()
 
