9f9eff248fb47b6ceac9e7b7d1e81a4e8db498e8
Charles <charles@mutuallyassured.com>
2020-10-17 12:27:14 -0700

    I got the ball to bounce off the paddle and pay attention to the boundaries. So far so good, but I need to make it go faster. I think I'm going to spend some time refactoring this to separate the files into separate classes, then I'm going to add in some tiles to hit.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 BALL_STATE_READY = "ready"
-BALL_STATE_FIRING = "firing"
+BALL_STATE_MOVE = "move"
 
 DIRECTION_NONE = "none"
 DIRECTION_LEFT = "left"
@@ -28,8 +28,8 @@ class Ball(object):
         self.y = 0
         self.x_direction = DIRECTION_NONE
         self.y_direction = DIRECTION_UP
-        self.x_speed = 300
-        self.y_speed = 100
+        self.x_speed = 400
+        self.y_speed = 250
 
     def draw(self):
         pygame.draw.circle(self.screen, (227, 232, 41), (int(self.x), int(self.y)), self.radius)
@@ -37,7 +37,7 @@ class Ball(object):
     def move(self, time_delta):
 
         # First, let's GTFO if we are already doing something
-        if self.state is not BALL_STATE_FIRING:
+        if self.state is not BALL_STATE_MOVE:
             return None
 
         if self.x_direction == DIRECTION_LEFT:
@@ -54,7 +54,7 @@ class Ball(object):
         self.y += time_delta * pixels_to_move_y
 
     def moveCenter(self, new_x):
-        
+        pass
 
 
 class Paddle(object):
@@ -65,7 +65,7 @@ class Paddle(object):
         self.screen = screen
         self.speed = 300
         self.state = PADDLE_STATE_STILL
-        self.width = 200
+        self.width = 150
         self.height = 25
         self.x = 0
         self.y = 0
@@ -73,11 +73,9 @@ class Paddle(object):
     def draw(self):
         self.screen.blit(self.image, (self.x, self.y))
 
-    def move(self, time_delta):
+    def move(self, time_delta, direction):
 
-        # Check the direction
-        if self.state == PADDLE_STATE_STILL:
-            return
+        self.direction = direction
 
         if self.direction == DIRECTION_LEFT:
             pixels_to_move_x = -self.speed
@@ -87,9 +85,22 @@ class Paddle(object):
         self.x += time_delta * pixels_to_move_x
 
 
+class Player(object):
+
+    def __init__(self, screen):
+        self.lives = 3  # Always start with three lives
+        self.screen = screen
+
+    def takeDamage(self, amount):
+        self.lives -= amount
+
+
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+
+# create the player
+player = Player(screen)
 
 # create the paddle
 paddle = Paddle(screen)
@@ -112,34 +123,44 @@ while True:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                ball.state = BALL_STATE_FIRING
-        elif event.type == pygame.KEYUP:
-            paddle.state = PADDLE_STATE_STILL
+                # Fire the ball
+                ball.state = BALL_STATE_MOVE
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        paddle.direction = DIRECTION_LEFT
-        paddle.state = PADDLE_STATE_MOVE
-    elif keys[pygame.K_RIGHT]:
-        paddle.direction = DIRECTION_RIGHT
-        paddle.state = PADDLE_STATE_MOVE
+    if keys[pygame.K_LEFT] and paddle.x > 0:
+        paddle.move(time_delta, DIRECTION_LEFT)
+    elif keys[pygame.K_RIGHT] and paddle.x <= SCREEN_WIDTH - paddle.width:
+        paddle.move(time_delta, DIRECTION_RIGHT)
 
     # FIll up the background
     screen.fill((25, 25, 25))
 
-    # Check the boundaries. Soon this will be with other sprites, not just the edge of the game
-    if ball.x <= 0:
-        ball.x_direction = DIRECTION_RIGHT
-    elif ball.x >= SCREEN_WIDTH - ball.radius:
-        ball.x_direction = DIRECTION_LEFT
+    if ball.state == BALL_STATE_MOVE:
+        # Check the boundaries for the ball. Soon this will be with other sprites, not just the edge of the game
+        if ball.x <= 0:
+            ball.x_direction = DIRECTION_RIGHT
+        elif ball.x >= SCREEN_WIDTH - ball.radius:
+            ball.x_direction = DIRECTION_LEFT
 
-    if ball.y <= 0:
-        ball.y_direction = DIRECTION_DOWN
-    elif ball.y >= SCREEN_HEIGHT - ball.radius:
-        ball.y_direction = DIRECTION_UP
+        # If the ball is off the map, send it back down. TODO: REPLACE WITH TILES
+        if ball.y <= 0:
+            ball.y_direction = DIRECTION_DOWN
+
+        #  First, check to see if the bottom of the ball is the same as the top of the paddle.
+        if ball.y + ball.radius >= paddle.y:
+
+            #  If so, then check to see if the x of the ball is within in the x_width of the paddle
+            if ball.x > paddle.x and ball.x < paddle.x + paddle.width:
+
+                # it is, so we need to reverse the course of the ball.
+                ball.y_direction = DIRECTION_UP
+
+                # and we need to figure out the right degree in which to bounce, and adjust the X and Y speeds accordingly
+                x_axis = ball.x - paddle.x
+                ball.x_speed = x_axis
 
     ball.move(time_delta)
-    paddle.move(time_delta)
+
     paddle.draw()
     ball.draw()
 
